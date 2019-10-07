0ad9d4f0102cc379395a5b20c25b0ab1d2f897dc
Antony Cherkun <>
2019-10-07 19:14:38 +0300

    initial commit

---
+++ BuilderAITask/BuilderAITask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BuilderAITask
+//
+//  Created by User on 10/7/19.
+//  Copyright © 2019 aCherkun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
